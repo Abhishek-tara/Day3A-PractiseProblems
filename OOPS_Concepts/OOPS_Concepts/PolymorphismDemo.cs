ef33236fd54ee5cf5027fc0f74f9ad8e12fef0d6
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-29 13:26:22 +0530

    Add: polymorphism demo

---
+++ OOPS_Concepts/OOPS_Concepts/PolymorphismDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Concepts
+{
+    public class PolymorphismDemo
+    {
+        public void SnakeSound()
+        {
+            Console.WriteLine("sssssssshh");
+        }
+    }
+
+    public class Dog : PolymorphismDemo
+    {
+        public void SnakeSound()
+        {
+            Console.WriteLine("Bow Bow");
+        }
+    }
+}
--- OOPS_Concepts/OOPS_Concepts/Program.cs
+++ OOPS_Concepts/OOPS_Concepts/Program.cs
@@ -5,10 +5,16 @@
         public static void Main(string[] args)
         {
             // Objects of a class
-            Object  obj = new Object();
-            obj.name = "Bruce";
-            obj.age = 25;
-            obj.Display();
+            //Object  obj = new Object();
+            //obj.name = "Bruce";
+            //obj.age = 25;
+            //obj.Display();
+
+            //Polymorphism
+            PolymorphismDemo obj = new PolymorphismDemo();
+            obj.SnakeSound();
+            Dog dog = new Dog();
+            dog.SnakeSound();
             
 
         }
