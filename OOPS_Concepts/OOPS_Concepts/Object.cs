8da4ae500e53b645a2a5ef07b16ba6fa776621b1
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-29 13:16:39 +0530

    Add: Objects and classes

---
+++ OOPS_Concepts/OOPS_Concepts/Object.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Concepts
+{
+    public class Object
+    {
+        public string name { get; set; }
+        public int age;
+        public void Display()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+        }
+    }
+}
---
+++ OOPS_Concepts/OOPS_Concepts/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace OOPS_Concepts
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Objects of a class
+            Object  obj = new Object();
+            obj.name = "Bruce";
+            obj.age = 25;
+            obj.Display();
+            
+
+        }
+    }
+}
